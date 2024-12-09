264ef0206a2f13a8d1215ccace7e8c50d8034980
denev6 <dev.seongjin@gmail.com>
2024-12-09 19:49:17 +0900

    12/09

--- study-pai/316_종복문자제거.py
+++ study-pai/316_종복문자제거.py
@@ -15,8 +15,8 @@ class Solution:
 
             while stack and char < stack[-1] and counter[stack[-1]] > 0:
                 # if (char exists in the stack)
-                # and (char is lexicographically smaller than the last char in stack)
-                # and (there is same char not being seen yet.)
+                # and (char is lexicographically smaller than the last char in the stack)
+                # and (there is a same char not being seen yet.)
 
                 # then remove the one added in the stack(answer)
                 # to append a new one later.
